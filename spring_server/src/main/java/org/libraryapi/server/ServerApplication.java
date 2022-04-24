392f047b95a0c09233b834e6657d0dca860c863a
santanaj <jackie.santana@biworldwide.com>
2022-04-23 21:39:03 -0500

    Jackie: fixed json in java server

--- spring_server/src/main/java/org/libraryapi/server/ServerApplication.java
+++ spring_server/src/main/java/org/libraryapi/server/ServerApplication.java
@@ -1,5 +1,5 @@
 package org.libraryapi.server;
-
+import org.json.simple.JSONObject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,8 +7,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.ArrayList;
 
 @SpringBootApplication
@@ -16,56 +14,35 @@ import java.util.ArrayList;
 
 public class ServerApplication {
 	
-	
-
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
-		
-		
 	}
 	
 	@CrossOrigin()
 	@GetMapping("/api/bookshelf")
 	public Object bookShelfResponse(@RequestParam(value = "name", defaultValue = "book") String name) {
-	
-//		Data Types
-//		Books
-		Map<Object, Object>books = new HashMap<>();
-		
-//		Authors
-		Map<String, String>author_robert_greene = new HashMap<>();
-		Map<String, String>author_napoleon_hill = new HashMap<>();
+		JSONObject books = new JSONObject();
+		JSONObject authors = new JSONObject();
+		JSONObject author_robert_greene = new JSONObject();
+		JSONObject author_napoleon_hill = new JSONObject();
 		
 //		Titles under Authors
 		ArrayList<String> napolean_hill_titles = new ArrayList<String>();
 		ArrayList<String> robert_greene_titles = new ArrayList<String>();
-
-//		Maps & Arrays
 		
-//		Titles
-//		Robert Greene Titles
-		robert_greene_titles.add("48  Laws of Power");
-		robert_greene_titles.add("Human nature");
-//		Napolean Hill Titles
+//		Robert greene Titles Arrays
+	 	robert_greene_titles.add("Human Nature");
+	 	robert_greene_titles.add("48 Laws of Power");
+	 	
+//		Napolean Hill Titles Arrays
 		napolean_hill_titles.add("Think and Grow Rich");
-		
-//		Authors
-		author_robert_greene.put("Author", "Robert greene");
-		author_napoleon_hill.put("Author", "Napolean Hill");
-	    
-//	    Books
-//	    book 1
-	    books.put(author_robert_greene, robert_greene_titles);
-//	    book 2
-	  books.put(author_napoleon_hill, napolean_hill_titles);
+
+//		Creating the JSON Object
+		 books.put("authors", authors);
+	     authors.put("Robert greene", robert_greene_titles);
+	     authors.put("Napolean Hill", napolean_hill_titles);
 
 		return books;
 	}
 	
 }
-
-// structure code like https://www.baeldung.com/spring-cors & https://www.geeksforgeeks.org/spring-boot-code-structure/#:~:text=There%20is%20no%20specific%20layout,divide%20the%20project%20into%20modules.
-//https://www.youtube.com/watch?v=O_XL9oQ1_To  
-//https://www.youtube.com/watch?v=H62Jfv1DJlU
-//figure out how to get key value in react & also wrap different authors inside an `authors` MAP
-//Deserialize JSON hashmap in react
